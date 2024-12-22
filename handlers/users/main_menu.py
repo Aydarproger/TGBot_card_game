8a58b08c29aa170c4fd3dc1a37d3ff5a75742141
Aydar <mr.bagamolov@vk.com>
2024-12-22 14:26:34 +0500

    Add files via upload

---
+++ handlers/users/main_menu.py
@@ -0,0 +1,31 @@
+from aiogram.types import Message, InputFile
+
+from config import cabinet_photo
+from data.functions.db import get_user
+from filters.filters import IsPrivate
+from keyboards.inline.other_keyboards import cabinet_keyboard
+from loader import dp, bot
+from texts import cabinet_text
+
+
+@dp.message_handler(IsPrivate(), text="🧑‍💻Кабинет")
+async def game_menu(message: Message):
+    if get_user(message.chat.id) != None:
+        await message.answer(cabinet_text(get_user(message.chat.id)),
+                             reply_markup=cabinet_keyboard())
+
+
+@dp.message_handler(IsPrivate(), text="🎲 Чат 🎲")
+async def chat(message: Message):
+    await message.answer("Чат ➖ https://t.me/slivmens")
+
+@dp.message_handler(IsPrivate(), text="🖥 Помощь")
+async def help(message: Message):
+    await message.answer("По всем вопросам ➖ @slivmens")
+
+
+@dp.message_handler(IsPrivate(), text="💫 Отзывы 💫")
+async def chat(message: Message):
+    await message.answer("Отзывы ➖ https://t.me/slivmens")
+
+
